b5d7bb62a3f7e57c0148f61a8133f511add90e75
iccee0 <rouven@12i.ch>
2021-10-01 13:51:26 +0200

    :)

---
+++ PowerShell/mSum.ps1
@@ -0,0 +1,15 @@
+# Summierenq
+# Rouven
+# 01.10.2021/13:30
+# mSum.ps1
+
+function mSum ([int] $p1, $p2) {
+    [int] $r = $p1 + $p2
+    Write-Host "Result:" $r
+}
+
+<#
+PS C:\Users\vmadmin\Documents\git\M122> mSum 1 2
+
+Result: 3
+#>
---
+++ PowerShell/mSum2.0.ps1
@@ -0,0 +1,9 @@
+# Summierer mit Funktion mit return
+# Rouven
+# 01.10.2021/Zeit
+# mSum2.0.ps1
+
+function mSum ([int] $p1, $p2) {
+    [int] $r = $p1 + $p2
+    return $r
+}
