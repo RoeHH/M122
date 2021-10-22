c18517d42b370fdfed3b982a7a69a4e65c02c3a5
iccee0 <rouven@12i.ch>
2021-10-22 13:55:06 +0200

    ps isch scho ned so geil

--- PowerShell/sumFunctionWithArray.ps1
+++ PowerShell/sumFunctionWithArray.ps1
@@ -3,7 +3,7 @@
 # Friday: 22.October.2021 13:29
 # sumFunctionWithArray.ps1
 
-function mSumRet ([array]$arr) { #Wichtig 
+function mSumRet ([array]$arr) { #Wichtig type casting 
     return $arr[0] + $arr[1]
 } 
 $rr = mSumRet (50, 50) 
