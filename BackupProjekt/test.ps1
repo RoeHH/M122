9844ed6da5b8294e1146623e3266c59ece410559
iccee0 <rouven@12i.ch>
2021-12-17 13:26:21 +0100

    Done :)

--- BackupProjekt/test.ps1
+++ BackupProjekt/test.ps1
@@ -1,25 +1,39 @@
-
-function createHeadOfLogfile () {
-    "File Name:  $logfileDir"| writeLineToLogfile
-    ""| writeLineToLogfile
-    "Operation      Time        DateiName"| writeLineToLogfile 
-    "------------------------------------"| writeLineToLogfile
+function createHeadOfLogfile ([string]$baseDir, [string]$backupDir) {
+    "File Name:  $global:logfileDest"| Out-File -FilePath $logfileDest -Encoding UTF8
+    writeLineToLogfile " "
+    writeLineToLogfile "Copy from: $baseDir"
+    writeLineToLogfile "To:        $backupDir"
+    writeLineToLogfile " "
+    writeLineToLogfile "Operation     Time                   DateiName"  
+    writeLineToLogfile "----------------------------------------------"
 }
 
 function writeLineToLogfile ([string] $str) {
-    $str | Add-Content -Path C:\temp\log.txt
+    $str | Add-Content -Path $global:logfileDest -Encoding UTF8
 }
 
-function copyElementsInDirectory ([string]$dir) {
+function createLogsForElementsInDirectory ([string]$dir, [string]$fromBaseDir) {
     foreach ($file in (Get-ChildItem -Path $dir -File)) {
         $time = Get-Date -Format "dd.MM.yyyy HH:mm:ss"
-        $fileName = $file.Name
-        "Copying File $time $fileName" | writeLineToLogfile
+        $fileName = $fromBaseDir + "/" + $file.Name
+        writeLineToLogfile "Copy File     $time    $fileName"
     }
-    foreach ($file in (Get-ChildItem -Path $dir -Directory)) {
-        #Write-Host "Do kopieremer de ordener: $file"
-        copyElementsInDirectory $file
+    foreach ($folder in (Get-ChildItem -Path $dir -Directory)) {
+        $time = Get-Date -Format "dd.MM.yyyy HH:mm:ss"
+        $folderName = $fromBaseDir + "/" + $folder.Name
+        writeLineToLogfile "Copy Folder   $time    $folderName"
+        createLogsForElementsInDirectory $folder.FullName ($fromBaseDir += "/" + $folder.Name)
     }
 }
-copyElementsInDirectory C:\Users\vmadmin\Documents\lilalu
+
+function createBackup ([string]$baseDir, [string]$backupDir) {
+    Copy-Item $baseDir -Destination $backupDir -Recurse -Force  #Kopieren des Ordners
+    createHeadOfLogfile $baseDir $backupDir #Erstellen der Kopierlogdatei
+    $baseDirName = (Get-Item -Path $baseDir).BaseName #Ermitteln des Basisordners
+    $baseDirRelative = "./$baseDirName" #Ermitteln des Basisordners relativ zum aktuellen Verzeichnis
+    $global:logfileDest = $backupDir + "\" + $baseDirName + "Log.txt" #Speicherort der Kopierlogdatei
+    createLogsForElementsInDirectory $baseDir $baseDirRelative #Befühlen der Kopierlogdatei
+}
+
+createBackup "C:\Users\vmadmin\Documents\lilalu" "C:\Users\vmadmin\Documents\Backup"
 
